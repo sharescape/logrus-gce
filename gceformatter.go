e18d284de92387ab443dc2db3b05fff6fd4defed
Jeremiah <jmiller.v@gmail.com>
2021-05-13 17:25:14 -0600

    different strategy

--- gceformatter.go
+++ gceformatter.go
@@ -77,8 +77,8 @@ func getSkipLevel(level logrus.Level) (int, error) {
 	for i, pc := range stackSkipsCallers {
 		f := runtime.FuncForPC(pc)
 		if !strings.Contains(f.Name(), "github.com/sirupsen/logrus") {
-			stackSkips[level] = i + 1
-			return i + 1, nil
+			stackSkips[level] = i - 1
+			return i - 1, nil
 		}
 		i++
 	}
