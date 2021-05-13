e2241b98ac40af5f5c149b43ae664f131b3fcbcf
Christian Nieves <christian@nieves.io>
2021-05-13 11:26:33 -0600

    replace strings.Prefix with strings.Contains
    Function name will also include the original build path. Can't use
    prefix

--- gceformatter.go
+++ gceformatter.go
@@ -76,7 +76,7 @@ func getSkipLevel(level logrus.Level) (int, error) {
 	runtime.Callers(3, stackSkipsCallers)
 	for i, pc := range stackSkipsCallers {
 		f := runtime.FuncForPC(pc)
-		if strings.HasPrefix(f.Name(), "github.com/sirupsen/logrus") == true {
+		if strings.Contains(f.Name(), "github.com/sirupsen/logrus") == true {
 			continue
 		}
 		stackSkips[level] = i + 1
